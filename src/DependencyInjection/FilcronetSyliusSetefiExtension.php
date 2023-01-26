ff2fb13acd20450847d044c3d14db740d61c10d9
Filippo <f.crotti@filcronet.com>
2023-01-26 13:15:06 +0100

    debug

--- src/DependencyInjection/FilcronetSyliusSetefiExtension.php
+++ src/DependencyInjection/FilcronetSyliusSetefiExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class FilcronetSyliusSetefiExtension extends Extension
 {
@@ -18,7 +18,7 @@ final class FilcronetSyliusSetefiExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.yaml');
     }
